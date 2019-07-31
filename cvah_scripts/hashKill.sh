b0850f278469e6077bb2803667ae13ae6d35a727
shunnell <stephen.hunnell2@gmail.com>
2019-07-31 14:25:09 -0500

    modified:   hashKill.sh

--- cvah_scripts/hashKill.sh
+++ cvah_scripts/hashKill.sh
@@ -1,3 +1,6 @@
 for list in `ps -ef |grep hashBack |grep -v color |cut -d" " -f6`; do
    kill -9 ${list}
 done
+for list in `ps -ef |grep md5sum |grep -v color |cut -d" " -f6`; do
+   kill -9 ${list}
+done
