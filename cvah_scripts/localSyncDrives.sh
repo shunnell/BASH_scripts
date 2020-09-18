add0548de99418bd35c316f4fbe150c280cc329c
shunnell <stephen.hunnell2@gmail.com>
2020-09-18 11:52:06 -0500

    Update localSyncDrives.sh

--- cvah_scripts/localSyncDrives.sh
+++ cvah_scripts/localSyncDrives.sh
@@ -15,3 +15,18 @@ COUNTER=1
    done
    #printf "\nSource not mounted, or not found!"
 
+#### Consider re-writing this AS THE ABOVE MAY RESULT IN ERROR:
+
+# COUNTER=1
+
+#    # Consider putting in an echo of $SYNC_SRC and a read to confirm before performing
+#    for SYNC_DEST in $(df -kh |grep dev |grep Data |cut -d% -f2); do
+#       if [ ${COUNTER} -gt 1 ]; then
+#          rsync -Lruvt --delete --force ${SYNC_SRC}/ ${SYNC_DEST}
+#       fi
+#       SYNC_SRC=${SYNC_DEST}
+#       let "COUNTER=COUNTER+1"
+#    done
+#    break
+# #printf "\nSource not mounted, or not found!"
+
